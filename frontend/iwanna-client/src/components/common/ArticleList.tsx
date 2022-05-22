d6788859c7ff3e537d5695114f72d5e457fe9989
unknown <mns0327@naver.com>
2022-05-22 17:43:08 +0900

    feat:lipstick: PopConfirm(가이드 알림) 컴포넌트 추가. antd 디자인과 PopConfirm 컴포넌트중 어느 것을 사용할지 고려중

--- frontend/iwanna-client/src/components/common/ArticleList.tsx
+++ frontend/iwanna-client/src/components/common/ArticleList.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../views/LandingPage/LandingPage.scss';
 import {AiOutlineInfoCircle} from 'react-icons/ai';
+import {Button, notification} from 'antd';
 
 interface ArticleListProps {
   curCategory: string;
 }
 
+const articleListInfo: string =
+  "후속 기사를 원하시면 '신청' 버튼을 눌러주세요.";
+
+const openNotificationWithIcon = (text: string) => {
+  notification.open({
+    message: text,
+  });
+};
+
 const ArticleList: React.FC<ArticleListProps> = ({curCategory}) => {
+  const [guideIsClicked, setGuideIsClicked] = useState(false);
+
   return (
     <div className="article_list">
-      <h1>{curCategory}</h1>
-      <AiOutlineInfoCircle />
+      <div className="category_title">
+        <div>{curCategory}</div>
+        <Button
+          type="link"
+          onClick={() => openNotificationWithIcon(articleListInfo)}
+        >
+          <AiOutlineInfoCircle
+            className="article_list_guide"
+            onClick={() => setGuideIsClicked(!guideIsClicked)}
+          />
+        </Button>
+        {/* <PopConfirm guideIsClicked={guideIsClicked} /> */}
+      </div>
     </div>
   );
 };
---
+++ frontend/iwanna-client/src/components/common/PopConfirm.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../views/LandingPage/LandingPage.scss';
+
+interface PopConfirmProps {
+  guideIsClicked: boolean;
+}
+
+const PopConfirm: React.FC<PopConfirmProps> = ({guideIsClicked}) => {
+  const articleListInfo: string =
+    "후속 기사를 원하시면 '신청' 버튼을 눌러주세요.";
+
+  return (
+    <div>
+      <span className={guideIsClicked ? 'pop_confirm clamp' : 'pop_confirm'}>
+        {articleListInfo}
+      </span>
+    </div>
+  );
+};
+
+export default PopConfirm;
