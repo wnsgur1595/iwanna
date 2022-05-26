3cfbd6583b6dfdd2cff9e1308bddcf729c28a352
unknown <mns0327@naver.com>
2022-05-26 15:46:19 +0900

    :sparkles: 회원가입 및 로그인 state, input event handler 추가

--- frontend/iwanna-client/src/components/views/LoginPage/LoginPage.tsx
+++ frontend/iwanna-client/src/components/views/LoginPage/LoginPage.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './LoginPage.scss';
 
 const LoginPage = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const idChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+
+  const passwordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="login">
       <form>
-        <span>IWANNA</span>
-        <input placeholder="아이디" />
-        <input placeholder="비밀번호" />
+        <span className="service_title">IWANNA</span>
+        <input placeholder="아이디" value={id} onChange={idChangeHandler} />
+        <input
+          placeholder="비밀번호"
+          value={password}
+          onChange={passwordChangeHandler}
+        />
         <button>로그인</button>
       </form>
     </div>
--- frontend/iwanna-client/src/components/views/RegisterPage/RegisterPage.tsx
+++ frontend/iwanna-client/src/components/views/RegisterPage/RegisterPage.tsx
@@ -1,33 +1,77 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './RegisterPage.scss';
 
 const RegisterPage = () => {
+  const [name, setName] = useState('');
+  const [id, setId] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const nameChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const idChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+
+  const emailChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const passwordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const confirmPasswordChangeHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="register">
-      <form>
+      <form onSubmit={onSubmit}>
         <span className="service_title">회원가입</span>
         <span className="required">*필수 입력</span>
         <div>
           <span className="required_icon">*</span>
-          <input placeholder="이름" />
+          <input placeholder="이름" onChange={nameChangeHandler} value={name} />
         </div>
         <div>
           <span className="required_icon">*</span>
-          <input placeholder="아이디" />
+          <input placeholder="아이디" onChange={idChangeHandler} value={id} />
         </div>
         <div>
           <span className="required_icon">*</span>
-          <input placeholder="이메일" />
+          <input
+            placeholder="이메일"
+            onChange={emailChangeHandler}
+            value={email}
+          />
         </div>
         <div>
           <span className="required_icon">*</span>
-          <input placeholder="비밀번호" />
+          <input
+            placeholder="비밀번호"
+            onChange={passwordChangeHandler}
+            value={password}
+          />
         </div>
         <div>
           <span className="required_icon">*</span>
-          <input placeholder="비밀번호 확인" />
+          <input
+            placeholder="비밀번호 확인"
+            onChange={confirmPasswordChangeHandler}
+            value={confirmPassword}
+          />
         </div>
-        <button>회원가입</button>
+        <button type="submit">회원가입</button>
       </form>
     </div>
   );
