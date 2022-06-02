fde2d3a3978748f9872491ee1a29a09bd893c8bb
unknown <mns0327@naver.com>
2022-06-02 14:25:47 +0900

    :lipstick: AlarmModal Component & Style 추가

--- frontend/iwanna-client/src/components/views/Header/Sections/AlarmModal.tsx
+++ frontend/iwanna-client/src/components/views/Header/Sections/AlarmModal.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const AlarmModal = () => {
-  return <div>AlarmModal</div>;
+interface AlarmModalProps {
+  tempData: {
+    id: string;
+    title: string;
+  }[];
+  isModal: boolean;
+  setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AlarmModal: React.FC<AlarmModalProps> = ({
+  tempData,
+  isModal,
+  setIsModal,
+}) => {
+  return (
+    <div className="modal_alarm_container" onClick={() => setIsModal(!isModal)}>
+      <div className="modal_alarm">
+        <h2>새 소식</h2>
+        {tempData.length > 0 ? (
+          <ul className="modal_alarm_list">
+            {tempData.map((alarm) => (
+              <li>
+                신청하신 <span>{alarm.title}</span> 뉴스의 후속 뉴스가
+                보도되었습니다.
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <span>새로운 알림이 없습니다.</span>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AlarmModal;
--- frontend/iwanna-client/src/components/views/Header/Sections/RightMenu.tsx
+++ frontend/iwanna-client/src/components/views/Header/Sections/RightMenu.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useNavigate} from 'react-router';
 import '../Header.scss';
 import {BsFillBellFill} from 'react-icons/bs';
+import AlarmModal from './AlarmModal';
 
 interface RightMenuProps {
   isLogin: boolean;
   isUser: boolean;
 }
 
-const temp_data = [
+// const temp_data: {id: string; title: string}[] = [];
+const temp_data: {id: string; title: string}[] = [
   {
     id: 'a_1',
-    title:
-      '[인천] 인천여성경제인협회, 산불 이재민에 천백만 원성금 뉴스의 후속 뉴스가 보도되었습니다.',
+    title: '[인천] 인천여성경제인협회, 산불 이재민에 천백만 원 성금1',
   },
   {
     id: 'a_2',
-    title: `[이엔피] "이준석'강용석과 밥도 먹고 술도 많이 마신 사이'발인" 뉴스의 후속 뉴스가 보도되었습니다.`,
+    title: '[인천] 인천여성경제인협회, 산불 이재민에 천백만 원 성금2',
+  },
+  {
+    id: 'a_3',
+    title: '[인천] 인천여성경제인협회, 산불 이재민에 천백만 원 성금3',
+  },
+  {
+    id: 'a_4',
+    title: '[인천] 인천여성경제인협회, 산불 이재민에 천백만 원 성금4',
   },
 ];
 
 const RightMenu: React.FC<RightMenuProps> = ({isLogin, isUser}) => {
   const navigate = useNavigate();
+  const [isModal, setIsModal] = useState(false);
   const name = '박민석';
 
   if (!isLogin) {
     return (
       <div className="right_menu">
-        <ul>
+        <ul className="right_menu_item">
           <li onClick={() => navigate('/login')}>로그인</li>
           <li onClick={() => navigate('/choice')}>회원가입</li>
         </ul>
@@ -36,14 +46,25 @@ const RightMenu: React.FC<RightMenuProps> = ({isLogin, isUser}) => {
   } else {
     return (
       <div className="right_menu">
-        <ul>
-          <li>
+        <ul className="right_menu_item">
+          <li onClick={() => setIsModal(!isModal)}>
             {temp_data.length > 0 && <div className="heaer_icon_spot"></div>}
             <BsFillBellFill className="header_icon" />
           </li>
-          {isUser ? <li>{name}님</li> : <li>{name}기자님</li>}
+          {isUser ? (
+            <li className="header_user">{name}님</li>
+          ) : (
+            <li className="header_user">{name}기자님</li>
+          )}
           <li onClick={() => navigate('/')}>마이페이지</li>
         </ul>
+        {isModal && (
+          <AlarmModal
+            tempData={temp_data}
+            isModal={isModal}
+            setIsModal={setIsModal}
+          />
+        )}
       </div>
     );
   }
