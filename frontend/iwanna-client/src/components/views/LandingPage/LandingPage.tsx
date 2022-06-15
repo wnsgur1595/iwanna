af77ce5498a3a86da60b23da1530750f005de410
unknown <mns0327@naver.com>
2022-06-15 14:06:50 +0900

    :fire: 불필요한 파일 삭제

--- frontend/iwanna-client/src/components/views/LandingPage/LandingPage.tsx
+++ frontend/iwanna-client/src/components/views/LandingPage/LandingPage.tsx
@@ -4,10 +4,10 @@ import Banner from './Sections/Banner';
 import News from './Sections/News';
 
 const LandingPage: React.FC = () => {
-  const [userType, setUserType] = useState('reporter'); // 임시 props
+  const [userType, setUserType] = useState('user'); // 임시 props
   const [isLogin, setIsLogin] = useState(true);
 
-  return (
+  return ( 
     <div className="landing">
       <Banner />
       <News userType={userType} isLogin={isLogin} />
